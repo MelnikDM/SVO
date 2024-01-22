f1b086b38cfc578dc2968c7ad7b2768cf2b69f01
MelnikDM <63dmsk@gmail.com>
2024-01-22 16:13:33 +0300

    Update config.py

--- LA_ObjectDetection/SSD300_VGG16/config.py
+++ LA_ObjectDetection/SSD300_VGG16/config.py
@@ -2,15 +2,15 @@ import torch
 
 BATCH_SIZE = 16 # Increase / decrease according to GPU memeory.
 RESIZE_TO = 640 # Resize the image for training and transforms.
-NUM_EPOCHS = 40 # Number of epochs to train for.
+NUM_EPOCHS = 30 # Number of epochs to train for.
 NUM_WORKERS = 4 # Number of parallel workers for data loading.
 
 DEVICE = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
 # Training images and XML files directory.
-TRAIN_DIR = 'data/License Plate Recognition.v1-raw-images.voc/train'
+TRAIN_DIR = 'data/LA/train'
 # Validation images and XML files directory.
-VALID_DIR = 'data/License Plate Recognition.v1-raw-images.voc/valid'
+VALID_DIR = 'data/LA/valid'
 
 # Classes: 0 index is reserved for background.
 CLASSES = [
