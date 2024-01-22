a1ee7e1e6242f9cae62d3f19921acfb8338e18ef
MelnikDM <63dmsk@gmail.com>
2024-01-22 09:36:57 +0300

    Add tracking

--- LA_ObjectDetection/Faster_RCNN_ResNet50_maP/inference_video.py
+++ LA_ObjectDetection/Faster_RCNN_ResNet50_maP/inference_video.py
@@ -10,9 +10,17 @@ import matplotlib.pyplot as plt
 
 from models.create_fasterrcnn_model import create_model
 from utils.general import set_infer_dir
-from utils.annotations import inference_annotations, annotate_fps
-from utils.transforms import infer_transforms
+from utils.annotations import (
+    inference_annotations, 
+    annotate_fps, 
+    convert_detections,
+    convert_pre_track,
+    convert_post_track
+)
+from utils.transforms import infer_transforms, resize
 from torchvision import transforms as transforms
+from deep_sort_realtime.deepsort_tracker import DeepSort
+from utils.logging import log_to_json
 
 def read_return_video_data(video_path):
     cap = cv2.VideoCapture(video_path)
@@ -30,34 +38,76 @@ def parse_opt():
         help='path to input video',
     )
     parser.add_argument(
-        '-c', '--config', 
+        '--data', 
         default=None,
         help='(optional) path to the data config file'
     )
     parser.add_argument(
-        '-m', '--model', default=None,
+        '-m', '--model', 
+        default=None,
         help='name of the model'
     )
     parser.add_argument(
-        '-w', '--weights', default=None,
+        '-w', '--weights', 
+        default=None,
         help='path to trained checkpoint weights if providing custom YAML file'
     )
     parser.add_argument(
-        '-th', '--threshold', default=0.3, type=float,
+        '-th', '--threshold', 
+        default=0.3, 
+        type=float,
         help='detection threshold'
     )
     parser.add_argument(
-        '-si', '--show-image', dest='show_image', action='store_true',
+        '-si', '--show',  
+        action='store_true',
         help='visualize output only if this argument is passed'
     )
     parser.add_argument(
-        '-mpl', '--mpl-show', dest='mpl_show', action='store_true',
+        '-mpl', '--mpl-show', 
+        dest='mpl_show', 
+        action='store_true',
         help='visualize using matplotlib, helpful in notebooks'
     )
     parser.add_argument(
         '-d', '--device', 
         default=torch.device('cuda:0' if torch.cuda.is_available() else 'cpu'),
         help='computation/training device, default is GPU if GPU present'
+    )
+    parser.add_argument(
+        '-ims', '--imgsz', 
+        default=None,
+        type=int,
+        help='resize image to, by default use the original frame/image size'
+    )
+    parser.add_argument(
+        '-nlb', '--no-labels',
+        dest='no_labels',
+        action='store_true',
+        help='do not show labels during on top of bounding boxes'
+    )
+    parser.add_argument(
+        '--square-img',
+        dest='square_img',
+        action='store_true',
+        help='whether to use square image resize, else use aspect ratio resize'
+    )
+    parser.add_argument(
+        '--classes',
+        nargs='+',
+        type=int,
+        default=None,
+        help='filter classes by visualization, --classes 1 2 3'
+    )
+    parser.add_argument(
+        '--track',
+        action='store_true'
+    )
+    parser.add_argument(
+        '--log-json',
+        dest='log_json',
+        action='store_true',
+        help='store a json log file in COCO format in the output directory'
     )
     args = vars(parser.parse_args())
     return args
@@ -66,10 +116,13 @@ def main(args):
     # For same annotation colors each time.
     np.random.seed(42)
 
+    if args['track']: # Initialize Deep SORT tracker if tracker is selected.
+        tracker = DeepSort(max_age=30)
+
     # Load the data configurations.
     data_configs = None
-    if args['config'] is not None:
-        with open(args['config']) as file:
+    if args['data'] is not None:
+        with open(args['data']) as file:
             data_configs = yaml.safe_load(file)
         NUM_CLASSES = data_configs['NC']
         CLASSES = data_configs['CLASSES']
@@ -89,17 +142,18 @@ def main(args):
             CLASSES = data_configs['CLASSES']
         try:
             build_model = create_model[args['model']]
+            model, coco_model = build_model(num_classes=NUM_CLASSES, coco_model=True)
         except:
-            build_model = create_model['fasterrcnn_resnet50_fpn']
-        model = build_model(num_classes=NUM_CLASSES, coco_model=True)
+            build_model = create_model['fasterrcnn_resnet50_fpn_v2']
+            model, coco_model = build_model(num_classes=NUM_CLASSES, coco_model=True)
     # Load weights if path provided.
     if args['weights'] is not None:
         checkpoint = torch.load(args['weights'], map_location=DEVICE)
         # If config file is not given, load from model dictionary.
         if data_configs is None:
             data_configs = True
-            NUM_CLASSES = checkpoint['config']['NC']
-            CLASSES = checkpoint['config']['CLASSES']
+            NUM_CLASSES = checkpoint['data']['NC']
+            CLASSES = checkpoint['data']['CLASSES']
         try:
             print('Building from model name arguments...')
             build_model = create_model[str(args['model'])]
@@ -127,7 +181,10 @@ def main(args):
     out = cv2.VideoWriter(f"{OUT_DIR}/{save_name}.mp4", 
                         cv2.VideoWriter_fourcc(*'mp4v'), 30, 
                         (frame_width, frame_height))
-    RESIZE_TO = (frame_width, frame_height)
+    if args['imgsz'] != None:
+        RESIZE_TO = args['imgsz']
+    else:
+        RESIZE_TO = frame_width
 
     frame_count = 0 # To count total frames.
     total_fps = 0 # To get the final frames per second.
@@ -137,7 +194,8 @@ def main(args):
         # capture each frame of the video
         ret, frame = cap.read()
         if ret:
-            frame = cv2.resize(frame, RESIZE_TO)
+            orig_frame = frame.copy()
+            frame = resize(frame, RESIZE_TO, square=args['square_img'])
             image = frame.copy()
             image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
             image = infer_transforms(image)
@@ -161,12 +219,33 @@ def main(args):
             
             # Load all detection to CPU for further operations.
             outputs = [{k: v.to('cpu') for k, v in t.items()} for t in outputs]
+            # Log to JSON?
+            if args['log_json']:
+                log_to_json(frame, os.path.join(OUT_DIR, 'log.json'), outputs)
             # Carry further only if there are detected boxes.
             if len(outputs[0]['boxes']) != 0:
-                frame = inference_annotations(
-                    outputs, detection_threshold, CLASSES,
-                    COLORS, frame
+                draw_boxes, pred_classes, scores = convert_detections(
+                    outputs, detection_threshold, CLASSES, args
                 )
+                if args['track']:
+                    tracker_inputs = convert_pre_track(
+                        draw_boxes, pred_classes, scores
+                    )
+                    # Update tracker with detections.
+                    tracks = tracker.update_tracks(tracker_inputs, frame=frame)
+                    draw_boxes, pred_classes, scores = convert_post_track(tracks) 
+                frame = inference_annotations(
+                    draw_boxes, 
+                    pred_classes, 
+                    scores,
+                    CLASSES, 
+                    COLORS, 
+                    orig_frame, 
+                    frame,
+                    args
+                )
+            else:
+                frame = orig_frame
             frame = annotate_fps(frame, fps)
 
             final_end_time = time.time()
@@ -176,7 +255,7 @@ def main(args):
             print_string += f"Forward pass + annotation time: {forward_and_annot_time:.3f} seconds"
             print(print_string)            
             out.write(frame)
-            if args['show_image']:
+            if args['show']:
                 cv2.imshow('Prediction', frame)
                 # Press `q` to exit
                 if cv2.waitKey(1) & 0xFF == ord('q'):
